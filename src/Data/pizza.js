d035e8ef5b3d71f7fdd263f931b9dbf8d14437d3
Forhad <forhadsh1@gmail.com>
2021-06-25 03:36:36 +0600

    change some picture

--- src/Data/pizza.js
+++ src/Data/pizza.js
@@ -4,38 +4,41 @@ export const pizza = [
     name: "Garlic Chicken Pizza",
     price: 900,
     imageUrl:
-      "http://server.pizzainn.com.bd/image/cache/data/roasted-600x600.jpg",
+      "https://images.unsplash.com/photo-1604917877934-07d8d248d396?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=634&q=80",
   },
   {
     id: 2,
     name: "BBQ Chicken Pizza",
     price: 1300,
-    imageUrl: "http://server.pizzainn.com.bd/image/cache/data/bbq-600x600.jpg",
+    imageUrl:
+      "https://images.unsplash.com/photo-1565299624946-b28f40a0ae38?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=714&q=80",
   },
   {
     id: 3,
     name: "Special  Pizza",
     price: 1400,
-    imageUrl: "http://server.pizzainn.com.bd/image/cache/data/03-600x600.jpg",
+    imageUrl:
+      "https://images.unsplash.com/photo-1513104890138-7c749659a591?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1050&q=80",
   },
   {
     id: 4,
     name: "Chicken Classic Pizza",
     price: 1350,
-    imageUrl: "http://server.pizzainn.com.bd/image/cache/data/bbq-600x600.jpg",
+    imageUrl:
+      "https://images.unsplash.com/photo-1596818531679-96ef98b9a497?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=634&q=80",
   },
   {
     id: 5,
     name: "Chicken Fajita Pizza",
     price: 1450,
     imageUrl:
-      "http://server.pizzainn.com.bd/image/cache/data/Fajita-600x600.jpg",
+      "https://images.unsplash.com/photo-1458642849426-cfb724f15ef7?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1050&q=80",
   },
   {
     id: 6,
     name: "Gulliver Pizza",
     price: 2200,
     imageUrl:
-      "http://server.pizzainn.com.bd/image/cache/data/All%20season-600x600.jpg",
+      "https://images.unsplash.com/photo-1506354666786-959d6d497f1a?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1050&q=80",
   },
 ];
