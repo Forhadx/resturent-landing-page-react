d8aeffe162838c276a48114095794240b743c012
Forhad <forhadsh1@gmail.com>
2021-06-25 04:54:47 +0600

    add email js

--- src/Contact/Form/Form.js
+++ src/Contact/Form/Form.js
@@ -1,19 +1,46 @@
 import React from "react";
+import emailjs from "emailjs-com";
 
 import "./form.scss";
 
 const Form = () => {
+  const sendEmail = (e) => {
+    e.preventDefault();
+    emailjs
+      .sendForm(
+        "service_c2ks5vl",
+        "template_hcl5x6j",
+        e.target,
+        "user_L18B3q07iVbwaxhsaOq9R"
+      )
+      .then(
+        (result) => {
+          console.log(result.text);
+        },
+        (error) => {
+          console.log(error.text);
+        }
+      );
+    e.target.reset();
+  };
+
   return (
-    <form className="contactForm">
-      <input type="text" placeholder="enter your name" required />
-      <input type="email" placeholder="enter your email" required />
+    <form className="contactForm" onSubmit={sendEmail}>
+      <input type="text" name="name" placeholder="enter your name" required />
+      <input
+        type="email"
+        name="email"
+        placeholder="enter your email"
+        required
+      />
       <textarea
         type="text"
+        name="message"
         rows="5"
         placeholder="write your message"
         required
       ></textarea>
-      <button>Send message</button>
+      <button type="submit">Send message</button>
     </form>
   );
 };
